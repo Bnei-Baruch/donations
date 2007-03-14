d08811913371606863c8577d2e0b03bb4a1a1744
gshilin <gshilin@88e0a184-90cd-446e-838e-ca381c575986>
2007-03-14 02:37:18 +0000

    1. Window interface 2. Payments

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function OpenWin(link, t, w, h) {
+    win = new Window("new_win", {className:"darkX", title:t, url:link, width:w, height:h, resizable:true, minimizable:false, maximizable:false, wiredDrag:true, showEffectOptions:{duration:0.5}});
+    win.setDestroyOnClose();
+    //if (navigator.userAgent.indexOf("MSIE") > -1) {
+        win.showCenter(true);
+    //} else {
+        //win.setLocation(50, 20);
+        //win.show(true);
+    //}
+}
