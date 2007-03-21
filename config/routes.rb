2197aa53fbe6e52665bc7111c53aa30b3df48d48
gshilin <gshilin@88e0a184-90cd-446e-838e-ca381c575986>
2007-03-21 17:35:23 +0000

    All parameters are now part of path

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,7 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id.:format'
   map.connect ':controller/:action/:id'
+  map.connect ':controller/:action/cur_action/:cur_action'
+  map.connect ':controller/:action/completed/:completed'
   map.empty '', :controller => 'user'
 end
