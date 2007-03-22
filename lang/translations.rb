517c03c109f69c650b4ee403ea04bfcd8bdd7b3f
dimap <dimap@88e0a184-90cd-446e-838e-ca381c575986>
2007-03-22 05:00:54 +0000

    Localization

--- lang/translations.rb
+++ lang/translations.rb
@@ -1,11 +1,18 @@
   Localization.define('rus') do |l|
  
-    l.store 'More', 'RU More'
-    l.store 'Bnei Baruch Association', 'RU Bnei Baruch Association'
-    
+    l.store 'Home', 'RU Home'
+    l.store 'About Bnei Baruh', 'RU About Bnei Baruh'
+    l.store 'Why Now', 'RU Why Now'
+    l.store "Contact us", "RU Contact us"
+    l.store 'FAQ', 'RU FAQ'
+    l.store 'Privacy and Security', 'RU Privacy and Security'
+    l.store 'Home', 'RU Home'
     l.store "Projects' History", "RU Projects' History"
     l.store 'Projects and Expenses', 'RU Projects and Expenses'
     l.store "Donors' list", "RU Donors' list"
+
+    l.store 'More', 'RU More'
+    l.store 'Bnei Baruch Association', 'RU Bnei Baruch Association'
 
     l.store 'Name', 'RU Name'
     l.store 'Description', 'RU Description'
@@ -19,5 +26,46 @@
     l.store 'Amount', 'RU Amount'
     l.store 'Anonymous', 'RU Anonymous'
  
+    l.store 'Message', 'RU Message'
+    l.store 'Date', 'RU Date'
+
+    l.store 'Thank you for your donation.', 'RU Thank you for your donation.'
+    l.store 'Transaction completed successfully.', 'RU Transaction completed successfully.'
+
+    l.store 'Payment', 'RU Payment'
+    l.store 'WebMoney', 'RU WebMoney'
+    l.store 'PayPal', 'RU PayPal'
+    l.store 'Currency', 'RU Currency'
+    l.store 'Shekels', 'RU Shekels'
+    l.store 'US Dollars', 'RU US Dollars'
+    l.store 'Payment method', 'RU Payment method'
+    l.store 'Regular Credit', 'RU Regular Credit'
+    l.store 'Installments', 'RU Installments'
+    l.store 'Credit Card Information', 'RU Credit Card Information'
+    l.store 'Credit Card Payment', 'RU Credit Card Payment'
+    l.store 'First Name', 'RU First Name'
+    l.store 'Last Name', 'RU Last Name'
+    l.store 'Credit Card Number', 'RU Credit Card Number'
+    l.store 'Credit Card', 'RU Credit Card'
+    l.store 'Expiration Date', 'RU Expiration Date'
+    l.store 'ID', 'RU ID'
+    l.store 'CVV Number', 'RU CVV Number'
+    l.store 'Contact Information', 'RU Contact Information'
+    l.store 'Publish my name', 'RU Publish my name'
+    l.store 'Email Address', 'RU Email Address'
+
+    l.store 'Submit', 'RU Submit'
+    l.store 'Reset', 'RU Reset'
+    l.store 'Yes', 'RU Yes'   
+    l.store 'No', 'RU No'
+    l.store 'Details', 'RU Details'
+
+    l.store 'Send Email Now', 'RU Send Email Now'
+    l.store 'Send Email', 'RU Send Email'
+    l.store 'Your name', 'RU Your name'
+    l.store 'Your email', 'RU Your email'
+    l.store 'Comments', 'RU Comments'
+    l.store 'Send', 'RU Send'
+
   end
  
