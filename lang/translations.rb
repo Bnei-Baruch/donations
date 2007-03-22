3045ffb250baee662df26aee84b56d28ef71a61a
dimap <dimap@88e0a184-90cd-446e-838e-ca381c575986>
2007-03-22 02:24:33 +0000

    Localization plugin

---
+++ lang/translations.rb
@@ -0,0 +1,23 @@
+  Localization.define('rus') do |l|
+ 
+    l.store 'More', 'RU More'
+    l.store 'Bnei Baruch Association', 'RU Bnei Baruch Association'
+    
+    l.store "Projects' History", "RU Projects' History"
+    l.store 'Projects and Expenses', 'RU Projects and Expenses'
+    l.store "Donors' list", "RU Donors' list"
+
+    l.store 'Name', 'RU Name'
+    l.store 'Description', 'RU Description'
+	
+    l.store 'Monthly Cost', 'RU Monthly Cost'
+    l.store 'Fixed Cost',   'RU Fixed Cost'
+    l.store 'Start Date',   'RU Start Date'
+    l.store 'End date',     'RU End date'
+
+    l.store 'Donation date', 'RU Donation date'
+    l.store 'Amount', 'RU Amount'
+    l.store 'Anonymous', 'RU Anonymous'
+ 
+  end
+ 
