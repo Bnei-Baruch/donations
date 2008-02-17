50d4d2de8ec1717cfa3d5e7e496262b545797212
dimap <dimap@88e0a184-90cd-446e-838e-ca381c575986>
2008-02-17 18:46:14 +0000

    Admin Search

--- app/controllers/admin_donor_controller.rb
+++ app/controllers/admin_donor_controller.rb
@@ -4,6 +4,11 @@ class AdminDonorController < ApplicationController
 
   before_filter :authorize
 
+  auto_complete_for :donor, :name
+  def auto_complete_for_donor_name
+    auto_complete_responder_for_contacts params["donor"]["name"]
+  end
+
   def index
     list
     render :action => 'list'
@@ -53,4 +58,43 @@ class AdminDonorController < ApplicationController
     Donor.find(params[:id]).destroy
     redirect_to :action => 'list'
   end
+
+  def search_pay
+    donors = Donor.find(:all)
+	
+	@countries = ((Array.new(donors.size) { |i| donors[i].country }).uniq).sort
+	@dates     = ((Array.new(donors.size) { |i| donors[i].created_at.to_date}).uniq).sort
+	#@amounts   = ((Array.new(donors.size) { |i| donors[i].sum_curr }).uniq).sort
+  end
+
+  def search_results
+
+	@name_prefix = params['donor']['name'] || ""
+	@date        = params['xxxDate'] || ""
+	@country     = params['xxxCountry'] || ""
+		
+	if (@country == "")
+		@donors = Donor.find(:all, 
+							 :conditions => [ "name ILIKE ?", @name_prefix + '%' ], 
+							 :order => 'name ASC')
+	else
+		@donors = Donor.find(:all, 
+							 :conditions => [ "name ILIKE ? AND country = ? ", @name_prefix + '%', @country ], 
+							 :order => 'name ASC')
+	end
+
+	if (@date != "")
+		@donors.delete_if { |donor| (donor.created_at.to_date.to_s != @date) }
+	end
+
+  end
+
+  private
+  def auto_complete_responder_for_contacts(value)
+    @donors = Donor.find(:all, 
+						 :conditions => [ "name ILIKE ?", value + '%'], 
+						 :order => 'name ASC')
+	render :inline => "<%= auto_complete_result @donors, 'name' %>"
+  end
+
 end
